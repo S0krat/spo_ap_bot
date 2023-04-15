813b550a1b3ffe52e79ba93b0b393b68cf83c9e2
S0krat <makoveev.lyu@edu.spbstu.ru>
2023-04-15 11:37:53 +0300

    База данных пользователей 1. Создана база данных пользователей с примитивным интерфейсом добавления. 2. Основной файл с кодом был разделён на пакеты.

---
+++ database/sqlite_db.py
@@ -0,0 +1,25 @@
+import sqlite3 as sq
+
+
+def sql_start():
+    global base, cur
+    base = sq.connect('users.db')
+    cur = base.cursor()
+    if base:
+        print("Database connected!")
+    base.execute("CREATE TABLE IF NOT EXISTS users ("
+                 "id int PRIMARY KEY, "
+                 "admin bool, "
+                 "name varchar(255))")
+    base.commit()
+
+
+def check_member(user_id):
+    cur.execute(f"SELECT admin FROM users WHERE id={user_id}")
+    return cur.fetchall()
+
+
+def add_new_member(data):
+    query = 'INSERT INTO users (id, admin, name) VALUES (?, 0, ?)'
+    cur.execute(query, data)
+    base.commit()
---
+++ handlers/admin.py
@@ -0,0 +1,97 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+import json
+from start_bot import config
+
+
+class AdminStatesGroup(StatesGroup):  # Класс, где хранятся все состояния
+    get_file_from_admin = State()
+    get_file_name_from_admin = State()
+    get_del_number_from_admin = State()
+
+
+async def create_command(message: types.Message):
+    if message.from_user.id in config['ADMIN']:
+        with open("json/file_ids.json", 'r', encoding='UTF-8') as files_json:
+            files = json.loads(files_json.read()).keys()
+        cons = "В данный момент в боте находятся файлы:\n"
+        for counter, name in enumerate(files):
+            cons += f"{counter + 1}. " + name + ".\n"
+        await message.answer(cons)
+        await message.answer("Кидай файл сюда!")
+        await AdminStatesGroup.get_file_from_admin.set()
+    else:
+        await message.answer("У тебя нет доступа к этому действию 😡")
+
+
+async def delete_command(message: types.Message):
+    if message.from_user.id in config['ADMIN']:
+        with open("json/file_ids.json", 'r', encoding='UTF-8') as files_json:
+            files = json.loads(files_json.read()).keys()
+        cons = "В данный момент в боте находятся файлы:\n"
+        for counter, name in enumerate(files):
+            cons += f"{counter + 1}. " + name + ".\n"
+        await message.answer(cons)
+        await message.answer("Напиши порядковый номер файла, который хочешь удалить.\n"
+                             "Если не хочешь ничего удалять напиши /cancel")
+        await AdminStatesGroup.get_del_number_from_admin.set()
+    else:
+        await message.answer("У тебя нет доступа к этому действию 😡")
+
+
+async def get_file_from_admin(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['document'] = message.document.file_id
+    await message.answer(
+        "Прекрасно! Теперь напиши название этого файла, которое будет отображаться в списке файлов.")
+    await AdminStatesGroup.next()
+
+
+async def gffa_exception(message: types.Message, state: FSMContext):
+    await message.answer("Я жду от тебя файл, а не признание в любви!\n"
+                         "Если хочешь отменить предыдущее действие напиши /cancel")
+
+
+async def get_file_name_from_admin(message: types.Message, state: FSMContext):
+    if message.content_type == types.ContentType.TEXT:
+        with open("json/file_ids.json", 'r', encoding='UTF-8') as files_json:
+            files = json.loads(files_json.read())
+        async with state.proxy() as data:
+            files.update({message.text: data['document']})
+        with open("json/file_ids.json", 'w', encoding='UTF-8') as files_json:
+            json.dump(files, files_json, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
+        await message.answer(f"Ура! Новый файл с названием '{message.text}' успешно создан!")
+        await state.finish()
+    else:
+        await message.answer("Я жду от тебя название файла!\nЕсли хочешь отменить создание файла, напиши /cancel")
+
+
+async def get_del_number_from_admin(message: types.Message, state: FSMContext):
+    if message.text.isdigit():
+        num = int(message.text)
+        with open("json/file_ids.json", 'r', encoding='UTF-8') as files_json:
+            files = json.loads(files_json.read())
+        if 0 < num <= len(files):
+            name = list(files.keys())[num - 1]
+            files.pop(name)
+            with open("json/file_ids.json", 'w', encoding='UTF-8') as files_json:
+                json.dump(files, files_json, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
+            await message.answer(f"Конспект '{name}' успешно удалён!")
+            await state.finish()
+        else:
+            await message.answer("Конспекта с таким номером нет в списке...\nНапиши команду /cancel для отмены.")
+    else:
+        await message.answer("Ты ввёл не число!\nНапиши команду \\cancel для отмены.")
+
+
+def register_handlers_admin(dispatcher: Dispatcher):
+    dispatcher.register_message_handler(create_command, commands=['create'])
+    dispatcher.register_message_handler(delete_command, commands=['delete'])
+    dispatcher.register_message_handler(get_file_from_admin, content_types=['document'],
+                                        state=AdminStatesGroup.get_file_from_admin)
+    dispatcher.register_message_handler(gffa_exception, content_types=['text'],
+                                        state=AdminStatesGroup.get_file_from_admin)
+    dispatcher.register_message_handler(get_file_name_from_admin, state=AdminStatesGroup.get_file_name_from_admin)
+    dispatcher.register_message_handler(get_del_number_from_admin, content_types=['text'],
+                                        state=AdminStatesGroup.get_del_number_from_admin)
